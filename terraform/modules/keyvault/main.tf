4162cf6d44659480184d4da4896a18ec9eb1e6ef
Thomas Teoh <me@tteoh.com>
2026-04-28 16:38:24 +1000

    chore: terraform fmt + add Terraform artifacts to .gitignore
    - Fixed duplicate required_providers block in backend.tf
    - Applied terraform fmt to modules/keyvault/main.tf and modules/storage/main.tf
    - Added .gitignore with Terraform-related entries (*.plan, .terraform/, .terraform.lock.hcl)

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -1,12 +1,12 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "this" {
-  name                      = "kv-${var.prefix}-${var.environment}"
-  location                  = var.location
-  resource_group_name       = var.resource_group_name
-  tenant_id                 = data.azurerm_client_config.current.tenant_id
-  sku_name                  = "standard"
-  enable_rbac_authorization = true
+  name                       = "kv-${var.prefix}-${var.environment}"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  enable_rbac_authorization  = true
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 }
--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_storage_account" "this" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  is_hns_enabled           = true  # ADLS Gen2 hierarchical namespace
+  is_hns_enabled           = true # ADLS Gen2 hierarchical namespace
 }
 
 locals {
